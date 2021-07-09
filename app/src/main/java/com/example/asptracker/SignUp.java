7f03e5944b3d29b1b71db573b88e33768a4c5aaf
ASPTracker <bsc-39-16@cc.ac.mw>
2021-07-09 17:36:02 +0200

    First prototype with sign in component working only

---
+++ app/src/main/java/com/example/asptracker/SignUp.java
@@ -0,0 +1,119 @@
+package com.example.asptracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.FetchData;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+public class SignUp extends AppCompatActivity {
+    //initializing text imputs from signin page
+    TextInputEditText textInputEditTextFullname, textInputEditTextUsername, textInputEditTextEmail, textInputEditTextPassword;
+    Button buttonSignUp;
+    TextView textViewLogin;
+    //object for the progressbar
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        //mapping user input and assign to variables variables
+        textInputEditTextFullname = findViewById(R.id.fullname);
+        textInputEditTextUsername = findViewById(R.id.username);
+        textInputEditTextEmail = findViewById(R.id.email);
+        textInputEditTextPassword = findViewById(R.id.password);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+        textViewLogin = findViewById(R.id.logintext);
+
+        progressBar = findViewById(R.id.progress);
+
+        //creating onclick listener for the button
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //variables for getting real input data
+                String fullname, username, email, password;
+                //capturing text
+                fullname = String.valueOf(textInputEditTextFullname.getText());
+                username = String.valueOf(textInputEditTextUsername.getText());
+                email = String.valueOf(textInputEditTextEmail.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
+
+                //checking whether there is an error in text fields before submiting
+                if(!fullname.equals("") && !username.equals("") && !email.equals("") && !password.equals("") ) {
+
+                    //Start ProgressBar first (Set visibility VISIBLE)\
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[4];
+                            field[0] = "fullname";
+                            field[1] = "username";
+                            field[2] = "password";
+                            field[3] = "email";
+                            //Creating array for data
+                            String[] data = new String[4];
+                            data[0] = "fullname";
+                            data[1] = "username";
+                            data[2] = "password";
+                            data[3] = "email";
+                            //From the emulator, 127.0.0.1 refers to the emulator itself - not your local machine. You need to use ip 10.0.2.2, which is bridged to your local machine.
+                            PutData putData = new PutData("http://10.0.2.2/LoginRegister/signup.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    //End ProgressBar (Set visibility to GONE) // after completing the process
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if(result.equals("Sign Up Success")){
+                                        //showing a toast message when loged successfully
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                        //creating intent to navigate to login activity
+                                        Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+                                   // Log.i("PutData", result);
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                }
+                else{
+                    //creating a toast to display an error message when the one or more fields is empty
+                    Toast.makeText(getApplicationContext(),"All fields required", Toast.LENGTH_SHORT).show();
+                }
+                textViewLogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+//end of codes
+            }
+        });
+
+    }
+}
