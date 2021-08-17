2271cdeb20da358235ba62ce569f5692f7540f9c
ASPTracker <bsc-39-16@cc.ac.mw>
2021-08-17 11:47:00 +0200

    shared preference added for logged user to log in automatically and navigation drawer is almost finished

--- app/src/main/java/com/example/asptracker/MainActivity.java
+++ app/src/main/java/com/example/asptracker/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button buttonConfig, buttonCoordinates, buttonLock, buttonControl;
+        TextView buttonConfig, buttonCoordinates, buttonLock, buttonControl;
+        ImageView image1, image2, image3, image4;
 
+        image1 = findViewById(R.id.imageView5);
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyLocation.class);
+                startActivity(intent);
+            }
+        });
         buttonConfig = findViewById(R.id.configure);
         buttonConfig.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), MyLocation.class);
                 startActivity(intent);
 
+            }
+        });
+        buttonConfig.setOnHoverListener(new View.OnHoverListener() {
+            @Override
+            public boolean onHover(View v, MotionEvent event) {
+                return false;
             }
         });
     }
